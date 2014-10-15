7adf407a3fd4e0d06d4ca5f75cf89d4ec7e9fbcf
Semenov Evgeniy <e7now@yandex.ru>
2014-10-15 21:53:30 +0700

    Изменил имена файлов и папок и добавил новый проект
    1. Пронумировал папки по порядку
    2. Добавил проект SeleniumWebDriver (папка 03 Selenium)

---
+++ 03 selenium/selenium/example.py
@@ -0,0 +1,23 @@
+# =*- coding: utf-8 -*-
+__author__ = 'User'
+
+from unittest import TestCase
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class SeleniumTest(TestCase):
+
+    def test_search(self):
+        driver = webdriver.Firefox()
+        driver.get("http://www.python.org")
+        assert "Python" in driver.title
+        elem = driver.find_element_by_name("q")
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+        driver.close()
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ 03 selenium/selenium/main.py
@@ -0,0 +1,24 @@
+# =*- coding: utf-8 -*-
+__author__ = 'SemenovEvgeniy'
+
+from unittest import TestCase
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class SeleniumTest(TestCase):
+    def test_EmailTest(self):
+        driver = webdriver.Firefox()
+        driver.get("http://www.pikabu.ru")
+        driver.find_element_by_id("login_block_btn_reg").click()
+        email = driver.find_element_by_id("rm_email")
+        email.clear()
+        email.send_keys("123@123")
+        driver.find_element_by_id("rm_pass").click()
+        element = driver.find_element_by_id("rm_email_err")
+        assert(element.text, "Неверный email", "Ошибка")
+        print(element.text)
+        driver.close()
+
+if __name__ == '__main__':
+    unittest.main()
