61c35b8d3f93fe4779a960036b5e335b970e2af9
Semenov Evgeniy <e7now@yandex.ru>
2014-10-15 23:01:00 +0700

    SeleniumWebDriver
    Добавил тест на коректный email

--- 03 selenium/selenium/main.py
+++ 03 selenium/selenium/main.py
@@ -7,17 +7,30 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 class SeleniumTest(TestCase):
-    def test_EmailTest(self):
+
+    def test_IncorrectEmail(self):
         driver = webdriver.Firefox()
         driver.get("http://www.pikabu.ru")
         driver.find_element_by_id("login_block_btn_reg").click()
         email = driver.find_element_by_id("rm_email")
         email.clear()
-        email.send_keys("123@123")
+        email.send_keys("@")
         driver.find_element_by_id("rm_pass").click()
         element = driver.find_element_by_id("rm_email_err")
-        assert "Неверный email", element.text
         driver.close()
+        assert u'Неверный email'==element.text
+
+    def test_CorrectEmail(self):
+        driver = webdriver.Firefox()
+        driver.get("http://www.pikabu.ru")
+        driver.find_element_by_id("login_block_btn_reg").click()
+        email = driver.find_element_by_id("rm_email")
+        email.clear()
+        email.send_keys("123@123.ru")
+        driver.find_element_by_id("rm_pass").click()
+        element = driver.find_element_by_id("rm_email_err")
+        driver.close()
+        assert element.text==""
 
 if __name__ == '__main__':
     unittest.main()
