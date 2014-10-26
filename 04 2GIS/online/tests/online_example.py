de372b12c0f7b50bd02a922500f173039d28b34a
Semenov Evgeniy <e7now@yandex.ru>
2014-10-26 14:02:36 +0600

    Изменения в теста�
    - переход на страницу 2gis вынесен в setUP
    - изменена тест поиска пути

--- 04 2GIS/online/tests/online_example.py
+++ 04 2GIS/online/tests/online_example.py
@@ -9,9 +9,9 @@ class SeleniumTest(TestCase):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(10)
         self.page = Page(self.driver)
+        self.page.open("http://2gis.ru")
 
     def test_link(self):
-        self.page.open("http://2gis.ru")
         request = u'кофе'
         self.page.search_bar.search(request)
         link = self.page.share_link.getLink()
@@ -19,12 +19,14 @@ class SeleniumTest(TestCase):
         self.assertEqual(request, self.page.search_bar.request)
 
     def test_way(self):
-        self.page.open("http://2gis.ru")
-        self.page.search_way_form.search(u'Новосибирск Блюхера 32/1', u'Сакко и Ванцетти, 42')
+        pointA = u'Новосибирск Блюхера 32/1'
+        pointB = u'Сакко и Ванцетти, 42'
+        self.page.search_way_form.search(pointA, pointB)
+        #self.page.search_way_form.search(u'Новосибирск Блюхера 32/1', u'Сакко и Ванцетти, 42')
         self.assertTrue(self.page.way_result.is_displayed)
 
     def tearDown(self):
-       self.driver.close()
+       self.driver.quit()
 
 if __name__ == '__main__':
     unittest.main()
