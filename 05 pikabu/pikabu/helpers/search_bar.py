608a6083bd8bb913b01ce85d4506e61f96be134e
Semenov Evgeniy <e7now@yandex.ru>
2014-10-26 14:52:54 +0600

    Изменения
    Добавлено
    - метод-свойство для вывода количества результатов
    - файл для экспериментов с функциями (пока там выделение числового
    значения из строки)

--- 05 pikabu/pikabu/helpers/search_bar.py
+++ 05 pikabu/pikabu/helpers/search_bar.py
@@ -4,10 +4,15 @@ class SearchBar(BaseComponent):
 
     selectors = {
         'input': '#search_menu',
-        'submit': '#serach_block > form > div > input.search_btn'
+        'submit': '#serach_block > form > div > input.search_btn',
+        'count_results': '#wrap > table > tbody > tr > td.main > table:nth-child(1) > tbody > tr > td:nth-child(2) > div > div > div'
     }
 
     def search(self, query):
         self.driver.find_element_by_css_selector(self.selectors['input']).clear()
         self.driver.find_element_by_css_selector(self.selectors['input']).send_keys(query)
         self.driver.find_element_by_css_selector(self.selectors['submit']).submit()
+
+    @property
+    def count_results(self):
+        return self.driver.find_element_by_css_selector(self.selectors['count_results']).text
